9e29a461a6b54f59e8f325ce7fe74dbe40039007
qq351469076 <d90159@163.com>
2018-09-18 10:15:20 +0800

    1
    1

--- delete_mysql_log.py
+++ delete_mysql_log.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
-from os import popen, getcwd, remove
+from os import popen, getcwd, remove, system
 from os.path import join
 from time import strftime, localtime
 from datetime import date, timedelta
@@ -9,16 +9,15 @@ from datetime import date, timedelta
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
 remove_list = []
-date = str(date.today() - timedelta(days=3))
 result = popen('ls --full-time | grep mysql-bin').readlines()
+
 for _ in result:
+    _ = _.split()    # 把当前变量变成数组
+    """['-rw-rw----', '1', 'my3306', 'mysql', '1073883249', '2018-08-22', '12:10:38.842014994', '+0800', 'mysql-bin.000600']"""
     if 'mysql-bin.index' not in _:
-        # ['-rw-rw----', '1', 'my3306', 'mysql', '1073883249', '2018-08-22', '12:10:38.842014994', '+0800', 'mysql-bin.000600']
-        if _.split()[5] < date:
-	    	remove_list.append(join(getcwd(), _.split()[-1]))
-if remove_list:
-    print remove_list
-    for temp in remove_list:
-		remove(temp)
+        remove_list.append(join(getcwd(), _[-1]))    # 把当前的名字存入到新数组
+
+if len(remove_list) > 3:    # 如果binlog大于3个,需要处理
+    for _ in remove_list[:-3]:    # 永远保留最后3个
+        remove(_)
